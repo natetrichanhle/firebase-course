b9f80f7e42999ced2a5482e2e85cb320ce780b74
Nate Trichanh <natetrichanh@gmail.com>
2023-04-25 16:35:23 -0700

    setting up authentication

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// this is what sets up authentication in the project
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWBOjMIexV7z8r9uOfNa-TfwskRyVZNf8",
+    authDomain: "fir-course-a2ec3.firebaseapp.com",
+    projectId: "fir-course-a2ec3",
+    storageBucket: "fir-course-a2ec3.appspot.com",
+    messagingSenderId: "862644385753",
+    appId: "1:862644385753:web:675c51730c064221954bc8",
+    measurementId: "G-S5B74DFSYV"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
